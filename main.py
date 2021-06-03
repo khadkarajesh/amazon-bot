24165a03626a6a9cce46e4805ea309fec538a1c6
rajesh <rajesh.k.khadka@gmail.com>
2021-06-04 00:49:22 +0200

    add automation

--- main.py
+++ main.py
@@ -1,6 +1,44 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Output
+from dotenv import load_dotenv
+
+from selenium_manager import SeleniumManager
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+load_dotenv()
+
+app.layout = html.Div(className="column", children=[
+    html.H1(children='''
+        Amazon Bot
+    '''),
+    html.Div(className="row", children=[
+        dcc.Input(id="product_name", type="text", placeholder="Enter product name", style={"margin-right": "10px"}),
+        html.Button('Search', id='btn_search', n_clicks=0),
+    ]),
+    html.Div(id='display_container', style={"margin-top": "16px"})
+], style={'justify-content': 'center',
+          'display': 'flex',
+          'align-items': 'center',
+          'flex-direction': 'column',
+          'height': '100vh'})
+
+
+@app.callback(Output('display_container', 'children'),
+              [dash.dependencies.Input('btn_search', 'n_clicks')],
+              [dash.dependencies.State('product_name', 'value')])
+def on_btn_click(n_clicks, value):
+    if n_clicks > 0 and value is not None:
+        selenium = SeleniumManager(base_url="https://www.amazon.com/")
+        selenium.login()
+        if selenium.search("cracking coding interview"):
+            return f"Product: {value} added successfully to cart"
+        else:
+            return "Couldn't add to cart"
 
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app.run_server(debug=True)
--- selenium_manager.py
+++ selenium_manager.py
@@ -1,18 +1,59 @@
+import os
+import time
+
 from selenium import webdriver
-from selenium.webdriver.common import keys
 
 
 class SeleniumManager:
     def __init__(self, base_url):
         self.base_url = base_url
         self.driver = webdriver.Chrome("./chromedriver")
+        self.success = False
 
-    def _initialize(self):
-        pass
-
-    def search(self):
+    def login(self):
         self.driver.get(self.base_url)
-        search_bar = self.driver.find_element_by_name("q")
-        search_bar.send_keys("getting started with python")
-        search_bar.send_keys(keys.Keys.RETURN)
-        search_bar.clear()
+        account_element = self.driver.find_element_by_id("nav-link-accountList")
+        account_element.click()
+
+        time.sleep(1)
+
+        email_element = self.driver.find_element_by_id("ap_email")
+
+        for char in os.environ.get('USERNAME'):
+            time.sleep(1)
+            email_element.send_keys(char)
+
+        continue_btn = self.driver.find_element_by_id("continue")
+        continue_btn.submit()
+
+        password_element = self.driver.find_element_by_id("ap_password")
+        for char in os.environ.get('PASSWORD'):
+            time.sleep(1)
+            password_element.send_keys(char)
+
+        sign_in_btn = self.driver.find_element_by_id("signInSubmit")
+
+        sign_in_btn.submit()
+
+        time.sleep(1)
+
+    def search(self, item_title):
+        # self.driver.get(self.base_url)
+
+        search_bar = self.driver.find_element_by_id("twotabsearchtextbox")
+
+        for key in item_title:
+            time.sleep(1)
+            search_bar.send_keys(key)
+
+        search_bar.submit()
+        time.sleep(3)
+
+        elements = self.driver.find_elements_by_css_selector(".a-link-normal.a-text-normal")
+        if len(elements) > 0:
+            elements[0].click()
+            time.sleep(4)
+            cart_btn = self.driver.find_element_by_id("add-to-cart-button")
+            cart_btn.submit()
+            self.success = True
+        return self.success
